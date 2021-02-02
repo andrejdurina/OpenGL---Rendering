079ee4689fc5d47fd30771d6aa93ac99606d98fe
andrejdurina <andrejdu.9@gmail.com>
2021-02-02 02:04:34 +0100

    Version 0.3, Texture loading fixed.

--- src/res/texture/texture.h
+++ src/res/texture/texture.h
@@ -22,7 +22,6 @@ class Texture
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     
-    
     int width=512,height=512,channels,dchannel;
 
     stbi_set_flip_vertically_on_load(true);
