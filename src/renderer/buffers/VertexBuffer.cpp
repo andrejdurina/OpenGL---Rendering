e5370c0ecf104e9404e918c194825d3759ac8bdf
andrejdurina <andrejdu.9@gmail.com>
2021-03-03 01:07:36 +0100

    Updated makefile.

--- src/renderer/buffers/VertexBuffer.cpp
+++ src/renderer/buffers/VertexBuffer.cpp
@@ -10,7 +10,7 @@ VertexBuffer::VertexBuffer(const void* data, unsigned int count)
     glGenBuffers(1, &RendererID);
     glBindBuffer(GL_ARRAY_BUFFER, RendererID);
     glBufferData(GL_ARRAY_BUFFER, 16 * sizeof(float) * Count , data, GL_STATIC_DRAW);
-    fprintf(stdout,"\n<VertexBuffer> Buffer size : %ld [bytes] \n ",32 * sizeof(float) );
+    fprintf(stdout,"\n<VertexBuffer> Buffer size : %ld [bytes] \n ",16 * sizeof(float) * Count );
 }
 VertexBuffer::~VertexBuffer()
 {
@@ -27,9 +27,9 @@ void VertexBuffer::Unbind() const
 }
 void VertexBuffer::AddObject(float* object )
 {   
-    int offset = 4 * sizeof(float) * GetCount() * 4;
-    unsigned int size =  4 * sizeof(float);
-    fprintf(stdout,"\n<VertexBuffer>Objects offset : %d  //[= 16]\n <VertexBuffer> Object no. : %d\n Object Size : %d \n",offset,GetCount() + 1,size);
+    int offset = 16 * sizeof(float) * GetCount();
+    unsigned int size =  16 * sizeof(float);
+    fprintf(stdout,"\n<VertexBuffer>Objects offset : %d  //[= 16]\n <VertexBuffer> Object no. : %d\n Object Size : %d \n",offset,GetCount(),size);
     Bind();
     glBufferSubData(GL_ARRAY_BUFFER, offset, size, object);
 }
